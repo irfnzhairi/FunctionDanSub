483fb80f976a96dc21796229c1cd0b5fe61ea098
Irfan Zuhairi Bin Jasni <18DDT19F1107@student.ptss.edu.my>
2021-04-21 11:43:58 +0800

    Latest

--- Function Dan Sub/Form1.vb
+++ Function Dan Sub/Form1.vb
@@ -1,30 +1,40 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    Private Sub S1()
         lblpapar.Text = "WELCOME"
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        S1()
+    End Sub
 
+    Private Sub S2()
         Dim strText As String
         strText = TextBox1.Text
         Label2.Text = strText
-
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        S2()
+    End Sub
+
+    Private Function F1()
+        Return "PTSS"
+    End Function
+
     Private Function Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label3 = F1()
+    End Function
 
-        Label3.Text = "PTSS"
-        Return Label3
+    Private Function F2()
+        Dim strText2 As String
+        strText2 = TextBox2.Text
 
+        Return Label4
     End Function
 
     Private Function Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-
-        Dim strText2 As String
-        strText2 = TextBox2.Text
-        Label4.Text = strText2
-        Return Label4
-
+        Label4.Text = F2()
     End Function
 End Class
 
